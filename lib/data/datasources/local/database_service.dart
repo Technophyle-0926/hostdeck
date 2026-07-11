a228186cb45e0bc112618578b28683666726a11e
Technophyle-0926 <meet15107@gmail.com>
2026-07-11 14:01:32 +0530

    chore: migrate to isar_community_flutter_libs and update generated database models

--- lib/data/datasources/local/database_service.dart
+++ lib/data/datasources/local/database_service.dart
@@ -1,4 +1,4 @@
-import 'package:isar/isar.dart';
+import 'package:isar_community/isar.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../models/host_account_model.dart';
 import '../../models/aggregated_build_model.dart';
